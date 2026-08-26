1f763f5b57c83c9817c982489ca50edd884fe788
jfbj54-arch <jfbj54@gmail.com>
2026-08-26 10:50:49 -0300

    Add files via upload

---
+++ SUlBAHIAGO/service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'sulbahiago-v1';
+const assetsToCache = [
+  './index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});

0a907aa360b9f69960e7b1ea77a1c1dc488d7e01
jfbj54-arch <jfbj54@gmail.com>
2026-09-11 12:50:36 -0300

    Update service-worker.js

--- SUlBAHIAGO/service-worker.js
+++ SUlBAHIAGO/service-worker.js
@@ -1,21 +1,5 @@
-const CACHE_NAME = 'sulbahiago-v1';
-const assetsToCache = [
-  './index.html',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(assetsToCache);
-    })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+const CACHE='sulbahiago-v51';
+const CORE=['./','./index.html','./manifest.webmanifest','./favicon.svg'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))))});
